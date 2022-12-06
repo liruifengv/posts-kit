8384167d7c10648001a0416ba442be5d0767b713
liruifengv <liruifeng1024@gmail.com>
2022-12-06 11:40:12 +0800

    feat: add promat to input username & password

--- main.ts
+++ main.ts
@@ -1,10 +1,34 @@
+import {
+  Input,
+  Secret,
+} from "https://deno.land/x/cliffy@v0.25.5/prompt/mod.ts";
+
 const filePath: string = Deno.args[0];
 
 if (!filePath) {
-  console.error("未指定 markdown 文件！");
-  Deno.exit(1);
+  throw Error("Not found args as the file path");
 }
 
 const markdown: string = Deno.readTextFileSync(filePath);
 
-console.log("读取 markdown 内容：\n", markdown);
+console.log("The markdown content is: \n", markdown);
+
+const username: string = await Input.prompt({
+  message: "Please input a username:",
+  default: "",
+});
+
+if (!username) {
+  throw Error("Username is required!");
+}
+
+const password: string = await Secret.prompt({
+  message: "Please input a password:",
+  default: "",
+});
+
+if (!password) {
+  throw Error("Password is required!");
+}
+
+console.log("Starting Login~");
